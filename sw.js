a285ad273cc3ccf67b4b900da6faf035dfec280b
asborozdin-a11y <asborozdin@gmail.com>
2026-09-14 15:24:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='ironlog-v1';
+const C='ironlog-v2';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))
   .then(()=>self.skipWaiting()));
