3e83899151e252d0f0f5721db5e75d72b9d2416d
asborozdin-a11y <asborozdin@gmail.com>
2026-09-15 19:06:08 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='ironlog-v20';
+const C='ironlog-v21';
 const FILES=['./','./index.html','./style.css','./data.js','./app.js','./train.js','./measure.js','./food.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',e=>{
